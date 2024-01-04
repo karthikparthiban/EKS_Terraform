7f30ff54460485c233e3c6b15e0d961b36bdf0ab
karthikparthiban <55242476+karthikparthiban@users.noreply.github.com>
2024-01-04 11:50:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 # Configure the AWS Provider
-#provider "aws" {
- # region = "us-east-1"  # Choose a valid AWS region from the supported list
-#}
-
+provider "aws" {
+  region = "ap-southeast-1"
+}
